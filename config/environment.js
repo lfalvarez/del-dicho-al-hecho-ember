d77e7dca78f788c71dc256ccb0acafd7185899f9
daquiroz <daquiroz@uc.cl>
2017-05-29 15:42:49 -0400

    config file changed

--- config/environment.js
+++ config/environment.js
@@ -1,131 +1,24 @@
 var governments =[{
-	"name": "Michelle Bachelet",
-	"years": {'start': 2014, 'end': 2018},
-	"color1": "#F64B57",
-	"color2": "#FF7E88",
-	"color3": "#FFB0B6",
-	"color4": "#FFE7E8",
+	"name": "Marcelo Crivella",
+	"years": {'start': 2017, 'end': 2020},
+	"color1": "#502466",
+	"color2": "#683682",
+	"color3": "#704687",
+	"color4": "#755884",
 	"studies": [{
 		"type": "Programa",
 		"img": "/img/bashelet-con-la-fiera.svg",
 		"color": "#FF7E88",
 		"year": 2016,
-		"version": "Programa de Gobierno",
-		"name": "Michelle Bachelet: Programa de Gobierno 2016",
-		"filename": "Bachelet-2014-2018_Programa-2017.csv",
-		"description": "del Programa de Gobierno, a tres años de haber asumido el poder.",
-		"visible": true,
-	}, {
-		"type": "Programa",
-		"img": "/img/bashelet-con-la-fiera.svg",
-		"color": "#FFB0B6",
-		"year": 2015,
-		"version": "Programa de Gobierno",
-		"name": "Michelle Bachelet: Programa de Gobierno 2015",
-		"filename": "Bachelet-2014-2018_Programa-2016.csv",
-		"description": "del Programa de Gobierno, a dos años de haber asumido el poder.",
-	}, {
-		"type": "Programa",
-		"img": "/img/bashelet-en-el-barquito.svg",
-		"color": "#FFE7E8",
-		"year": 2014,
-		"version": "Programa de Gobierno",
-		"name": "Michelle Bachelet: Programa de Gobierno 2014",
-    "filename": "Bachelet-2014-2018_Programa-2015.csv",
-		"description": "del Programa de Gobierno, a un año de haber asumido el poder.",
-	},{
-		"type": "Discurso",
-		"img": "/img/bashelet-con-la-fiera.svg",
-		"color": "#FDAAAF",
 		"in_landing": true,
-		"year": 2016,
-		"version": "Cuenta Pública",
-		"name": "Michelle Bachelet: Cuenta Pública 2016",
-		"filename": "Bachelet-2014-2018_Discurso-2017.csv",
-		"description": "de la Cuenta Pública 2016.",
+		"version": "Programa de Gobierno",
+		"name": "Evaluación Río de Janeiro 100 días",
+		"filename": "Rio100dias.csv",
+		"description": "Quanto cumpriu a gestão de Marcelo Crivella na Prefeitura do Rio em 100 dias de governo? Descubra em Dito e Feito Rio.",
 		"visible": true,
-		}, {
-		"type": "Discurso",
-		"img": "/img/bashelet-con-la-fiera.svg",
-		"color": "#FDAAAF",
-		"year": 2015,
-		"version": "Cuenta Pública",
-		"name": "Michelle Bachelet: Cuenta Pública 2015",
-		"filename": "Bachelet-2014-2018_Discurso-2016.csv",
-		"description": "de la Cuenta Pública 2015.",
-	}, {
-		"type": "Discurso",
-		"img": "/img/bashelet-en-el-avion.svg",
-		"color": "#FCCCD0",
-		"year": 2014,
-		"version": "Cuenta Pública",
-		"name": "Michelle Bachelet: Cuenta Pública 2014",
-    "filename": "Bachelet-2014-2018_Discurso-2015.csv",
-		"description": "de la Cuenta Pública 2014.",
-	}, ]
-}, {
-	"name": "Sebastián Piñera",
-	"years": {'start': 2010, 'end': 2014},
-	"color1": "#2ABAFD",
-	"color2": "#70D2FF",
-	"color3": "#B0E6FF",
-	"color4": "#DBF4FF",
-	"studies": [{
-		"type": "Programa",
-		"img": "/img/pinera-en-el-bicicleta.svg",
-		"color": "#2ABAFD",
-		"year": 2013,
-		"version": "Programa de Gobierno",
-		"name": "Sebastián Piñera: Programa de Gobierno 2013",
-		"filename": "Piñera-2010-2014_Programa-2014.csv",
-		"description": "del Programa de Gobierno, a cuatro años de haber asumido el poder.",
-	}, {
-		"type": "Programa",
-		"img": "/img/pinera-en-el-bicicleta.svg",
-		"color": "#70D2FF",
-		"year": 2012,
-		"version": "Programa de Gobierno",
-		"name": "Sebastián Piñera: Programa de Gobierno 2012",
-		"filename": "Piñera-2010-2014_Programa-2013.csv",
-		"description": "del Programa de Gobierno, a tres años de haber asumido el poder.",
-	}, {
-		"type": "Programa",
-		"img": "/img/pinera-en-el-auto.svg",
-		"color": "#B0E6FF",
-		"year": 2011,
-		"version": "Programa de Gobierno",
-		"name": "Sebastián Piñera: Programa de Gobierno 2011",
-		"filename": "Piñera-2010-2014_Programa-2012.csv",
-		"description": "del Programa de Gobierno, a dos años de haber asumido el poder.",
-	}, {
-		"type": "Discurso",
-		"img": "/img/pinera-en-el-bicicleta.svg",
-		"color": "#2ABAFD",
-		"year": 2013,
-		"version": "Cuenta Pública",
-		"name": "Sebastián Piñera: Cuenta Pública 2013",
-		"filename": "Piñera-2010-2014_Discurso-2014.csv",
-		"description": "de la Cuenta Pública 2013.",
-	}, {
-		"type": "Discurso",
-		"img": "/img/pinera-en-el-bicicleta.svg",
-		"color": "#67C7F4",
-		"year": 2012,
-		"version": "Cuenta Pública",
-		"name": "Sebastián Piñera: Cuenta Pública 2012",
-		"filename": "Piñera-2010-2014_Discurso-2013.csv",
-		"description": "de la Cuenta Pública 2012.",
-	}, {
-		"type": "Discurso",
-		"img": "/img/pinera-en-el-auto.svg",
-		"color": "#91CFEC",
-		"year": 2011,
-		"version": "Cuenta Pública",
-		"name": "Sebastián Piñera: Cuenta Pública 2011",
-		"filename": "Piñera-2010-2014_Discurso-2012.csv",
-		"description": "de la Cuenta Pública 2011.",
-	},]
-}];
+	}
+	]
+} ];
 module.exports = function(environment) {
   var orders = {
     1: 'primer',
@@ -151,7 +44,7 @@ module.exports = function(environment) {
     'Minería': 'img/i-min.svg',
     'Obras Públicas': 'img/i-obr.svg',
     'Pueblos Indígenas': 'img/i-pue.svg',
-    'Salud': 'img/i-sal.svg',
+    'Saúde': 'img/i-sal.svg',
     'Seguridad Ciudadana': 'img/i-seg.svg',
     'Trabajo y Previsión Social': 'img/i-tra.svg',
     'Transporte y Telecomunicaciones': 'img/i-tran.svg',
@@ -166,9 +59,9 @@ module.exports = function(environment) {
       }
     },
     'promise': {
-      'content': 'promesa',
+      'content': 'Promesa',
       'number': 'uid',
-      'title': 'titulo',
+      'title': 'Titulo',
       'ja-why': 'ja_porque',
       'jc-why': 'jn_porque',
       'coherence-level': 'escala_de_coherencia',
@@ -180,20 +73,20 @@ module.exports = function(environment) {
       ]
     },
     'bill': {
-      'name': 'boletin',
-      'title': 'titulo_proyecto',
-      'url': 'link',
+      'name': 'Identificador acto administrativo',
+      'title': 'Acto administrativo',
+      'url': 'Link',
       'justification': 'justificacion_avance',
       'year': 'Ano',
       'version': 'Version',
       'id': {
-        'fieldToGetIdFrom': 'boletin'
+        'fieldToGetIdFrom': 'Identificador acto administrativo'
       },
       'relationships':[
         "phase",
         "priority",
       ],
-      'chekIsEmpty': 'boletin',
+      'chekIsEmpty': 'Identificador acto administrativo',
     },
     'justification': {
       'justification': 'justificacion_avance',
@@ -226,35 +119,20 @@ module.exports = function(environment) {
     'columnName': 'nombre_avance',
     "phases":[
     {
-      'name': 'Primer Tramite',
-      'fullfilment': 40,
-    }, {
-      'name': 'Segundo Tramite',
-      'fullfilment': 70,
-    }, {
-      'name': 'Veto',
-      'fullfilment': 40,
-    }, {
-      'name': 'Insistencia',
-      'fullfilment': 40,
-    }, {
-      'name': 'Rechazado Retirado',
+      'name': 'Sem progresso',
       'fullfilment': 0,
     }, {
-      'name': 'Comisión Mixta',
-      'fullfilment': 70,
+      'name': 'Estágio inicial',
+      'fullfilment': 10,
     }, {
-      'name': 'Tribunal Constitucional',
-      'fullfilment': 100,
+      'name': 'Avanço marginal',
+      'fullfilment': 30,
     }, {
-      'name': 'Aprobación Presidencial',
-      'fullfilment': 90,
-    }, {
-      'name': 'Promulgado',
-      'fullfilment': 100,
+      'name': 'Prazo Perdido',
+      'fullfilment': 0,
     }
   ]};
-	var comparisonsAreVisible = true;
+	var comparisonsAreVisible = false;
 
   var ENV = {
     modulePrefix: 'ddah-ember',
